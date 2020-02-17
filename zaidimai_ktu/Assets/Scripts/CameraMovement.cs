7f0264651d774f5505f357a4ef791fdb90990bf9
Karolis <karolisb27@gmail.com>
2020-02-17 13:44:02 +0200

    Pradzia camera

---
+++ zaidimai_ktu/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3 realPos;
+    public Vector3 tempPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
