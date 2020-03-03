1b504cdac322ab5dc4f39ac10b363ecc8d04c618
Ezasu <niekas3310@gmail.com>
2020-03-03 09:10:13 +0200

    fix
    sds

--- zaidimai_ktu/Assets/Scripts/PlayerMovement.cs
+++ zaidimai_ktu/Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
-    Touch thisTouch;
-    Touch lastTouch;
-    float scrollSpeedX;
+    float scrollSpeedX, scrollSpeedY;
 
-    [Range(0, 10)]
+    int directionX, directionY;
+
+    [Range(0, 50)]
     public float MaxSpeed = 5f;
 
-    [Range(0, 1)]
+    [Range(0, 25)]
     public float SpeedMultiplier = 0.5f;
 
-    bool slowingDown = false;
+    [Range(0, 0.5f)]
+    public float SlowDownMultiplier = 0.1f;
+
+    [Range(0, 1000)]
+    public float MinimumScrollSpeedToMovePlayer = 0.4f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        //Debug.ClearDeveloperConsole();
+        scrollSpeedX = 0;
+        scrollSpeedY = 0;
     }
 
     // Update is called once per frame
@@ -30,50 +35,60 @@ public class PlayerMovement : MonoBehaviour
         if (Input.touchCount > 0)
         {
             var touch = Input.GetTouch(0);
-            scrollSpeedX = touch.deltaPosition.x / touch.deltaTime;
-            if (touch.phase == TouchPhase.Began)
-            {
-                thisTouch = touch;
-                lastTouch = touch;
-                return;
-            }
-            thisTouch = touch;
+            float newScrollSpeedX = touch.deltaPosition.x / touch.deltaTime * SpeedMultiplier * (-1);
+            float newScrollSpeedY = touch.deltaPosition.y / touch.deltaTime * SpeedMultiplier * (-1);
 
-            Debug.Log($"vel: {scrollSpeedX}");
+            if (newScrollSpeedX != 0)
+            //if (Mathf.Abs(newScrollSpeedX) > MinimumScrollSpeedToMovePlayer)
+            {
+                var newDirection = (newScrollSpeedX > 0 ? 1 : -1);
+                if (newDirection != directionX && Mathf.Abs(rb.velocity.y) > 1) 
+                { // jei scroll kryptis pakeista ir player juda (velocity > 1)
+                    scrollSpeedX *= 1.5f;
+                }
+                else
+                {
+                    directionX = newDirection;//newScrollSpeedX > 0 ? 1 : -1;
+                    if (Mathf.Abs(scrollSpeedX + newScrollSpeedX) > MaxSpeed)
+                        scrollSpeedX = MaxSpeed * directionX;
+                    else
+                        scrollSpeedX = newScrollSpeedX;
+                }
+
+            }
+            else
+                scrollSpeedX = 0;
+            //if (newScrollSpeedY != 0)
+            if (Mathf.Abs(newScrollSpeedY) > MinimumScrollSpeedToMovePlayer)
+            {
+                //if ((newScrollSpeedY > 0 ? 1 : -1) != directionY)
+                //{
+                //    newScrollSpeedY *= 15f;
+                //}
+                directionY = newScrollSpeedY > 0 ? 1 : -1;
+                if (Mathf.Abs(scrollSpeedY + newScrollSpeedY) > MaxSpeed)
+                    scrollSpeedY = MaxSpeed * directionY;
+                else
+                    scrollSpeedY = newScrollSpeedY; 
+            }
+            else
+                scrollSpeedY = 0;
+            //if (scrollSpeedY != scrollSpeedX && scrollSpeedY != 0)
+                rb.AddForce(new Vector2(scrollSpeedX * 10, scrollSpeedY * 15));
+            //rb.MovePosition((Vector2)transform.position + new Vector2(scrollSpeedX, scrollSpeedY) * Time.deltaTime);
+
+            //transform.Translate(new Vector3(scrollSpeedX, scrollSpeedY, 0) * Time.deltaTime);
+
+
 
 
             //rb.velocity = new Vector2(Mathf.Lerp(lastTouch.position.x, thisTouch.position.x, SpeedMultiplier), rb.velocity.y);
 
-            rb.velocity = new Vector2(SpeedMultiplier * scrollSpeedX, rb.velocity.y);
 
-            lastTouch = thisTouch;
-
-            //if (touch.phase == TouchPhase.Ended)
-            //{
-            //    slowingDown = true;
-            //}
-            slowingDown = true;
         }
-        else if (slowingDown)
+        if (Mathf.Abs(rb.velocity.x) > MaxSpeed)
         {
-            rb.AddForce(new Vector2((-1) * rb.velocity.x / 200, 0f));
-            //rb.velocity = new Vector2(rb.velocity.x / 2 , rb.velocity.y);
-            if (Mathf.Abs(rb.velocity.x) < 0.01f)
-            {
-                slowingDown = false;
-                Debug.ClearDeveloperConsole();
-                Debug.Log("slowed down");
-            }
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * MaxSpeed, rb.velocity.y);
         }
-
-        //if (Input.touchCount > 0)
-        //{
-        //    thisTouch = Input.GetTouch(0);
-        //    if (thisTouch.phase == TouchPhase.Began)
-        //    {
-        //        lastTouch = thisTouch;
-        //    }
-        //}
-
     }
 }
