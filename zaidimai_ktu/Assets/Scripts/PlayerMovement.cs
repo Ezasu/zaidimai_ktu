777c03265e973843657b6c37d393d17c04759a25
Ezasu <niekas3310@gmail.com>
2020-03-06 10:35:35 +0200

    a

--- zaidimai_ktu/Assets/Scripts/PlayerMovement.cs
+++ zaidimai_ktu/Assets/Scripts/PlayerMovement.cs
@@ -15,9 +15,6 @@ public class PlayerMovement : MonoBehaviour
     [Range(0, 25)]
     public float SpeedMultiplier = 0.5f;
 
-    [Range(0, 0.5f)]
-    public float SlowDownMultiplier = 0.1f;
-
     [Range(0, 1000)]
     public float MinimumScrollSpeedToMovePlayer = 0.4f;
 
@@ -58,13 +55,8 @@ public class PlayerMovement : MonoBehaviour
             }
             else
                 scrollSpeedX = 0;
-            //if (newScrollSpeedY != 0)
             if (Mathf.Abs(newScrollSpeedY) > MinimumScrollSpeedToMovePlayer)
             {
-                //if ((newScrollSpeedY > 0 ? 1 : -1) != directionY)
-                //{
-                //    newScrollSpeedY *= 15f;
-                //}
                 directionY = newScrollSpeedY > 0 ? 1 : -1;
                 if (Mathf.Abs(scrollSpeedY + newScrollSpeedY) > MaxSpeed)
                     scrollSpeedY = MaxSpeed * directionY;
@@ -73,22 +65,14 @@ public class PlayerMovement : MonoBehaviour
             }
             else
                 scrollSpeedY = 0;
-            //if (scrollSpeedY != scrollSpeedX && scrollSpeedY != 0)
-                rb.AddForce(new Vector2(scrollSpeedX * 10, scrollSpeedY * 15));
-            //rb.MovePosition((Vector2)transform.position + new Vector2(scrollSpeedX, scrollSpeedY) * Time.deltaTime);
-
-            //transform.Translate(new Vector3(scrollSpeedX, scrollSpeedY, 0) * Time.deltaTime);
-
-
-
-
-            //rb.velocity = new Vector2(Mathf.Lerp(lastTouch.position.x, thisTouch.position.x, SpeedMultiplier), rb.velocity.y);
-
-
+            rb.AddForce(new Vector2(scrollSpeedX * 10, scrollSpeedY * 17));
         }
         if (Mathf.Abs(rb.velocity.x) > MaxSpeed)
         {
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * MaxSpeed, rb.velocity.y);
         }
+
+        if (transform.position.y < -2)
+            transform.position = Vector2.zero;
     }
 }
