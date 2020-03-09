9b074b8601b5c3032d528955bf38d0a4247f77be
Karolis <karolisb27@gmail.com>
2020-03-09 11:27:31 +0200

    Material photos

--- zaidimai_ktu/Assets/Scripts/CameraMovement.cs
+++ zaidimai_ktu/Assets/Scripts/CameraMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -8,11 +9,9 @@ public class CameraMovement : MonoBehaviour
     Vector3 tempPos;
     public GameObject Player;
     float yPosition; //kameros y pozicija
-
     float fingerPressedY; // y koordinate, kur pirstas prisiliete prie ekrano
 
-    [Range(0,50)]
-    public float strenght = 4.7f;
+    float strenght = 4.7f;
 
     [Range(0, 20)]
     public float playerSize = 20f;
@@ -24,6 +23,7 @@ public class CameraMovement : MonoBehaviour
     public float getSpeed = 0.5f;
 
     float touchBeganAt = 0;
+    float camBeganAt = 0;
     float scrolledY = 0;
 
     // Start is called before the first frame update
@@ -35,60 +35,33 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Debug.Log(Player.transform.position.y);
         Vector3 laikinas = Player.transform.position;
         laikinas.x = laikinas.x + 1;
         laikinas.y = laikinas.y;
         RaycastHit2D hit = Physics2D.Raycast(laikinas, Vector3.down);
         float distance = Player.transform.position.y - hit.point.y - 1.8348f;
-        //Ray ray = new Ray(Player.transform.position, Vector3.down);
-        Debug.Log(hit.point.y);
+        //Debug.Log(hit.point.y);
         if (Input.touchCount > 0)
         {
             var touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
-                //touchBeganAt = Camera.main.ScreenToWorldPoint(touch.position).y;
                 touchBeganAt = touch.position.y;
-
-
-                //fingerPressedY = Player.transform.position.y - Camera.main.ScreenToWorldPoint(touch.position).y;
-                
+                camBeganAt = gameObject.transform.position.y;
             }
-            scrolledY = touch.position.y - touchBeganAt - playerSize;
-            scrolledY = scrolledY / 100 * strenght;
-            scrolledY = scrolledY - scrolledY / 2;
-            Debug.Log(scrolledY);
-            gameObject.transform.position = new Vector3(Player.transform.position.x, scrolledY + hit.point.y, -10);
-            //float pressedPos;
-            //pressedPos = (Screen.height / 2 - touch.position.y) / Screen.height * strenght - (touch.position.y / Screen.height * strenght) / 2;
-            //float skirtumas = pressedPos - yPosition;
-            ////Debug.Log("Skirtumas = " + skirtumas);
-            //yPosition = (float)(yPosition + skirtumas * getSpeed) * -1;
-            ////yPosition = fingerPressedY;
-            ////Debug.Log(yPosition);
-            //if (skirtumas > 0 && Mathf.Abs(pressedPos) > Mathf.Abs(yPosition))
-            //    yPosition = (float)(yPosition - pressedPos * 0.3);
-            //if (skirtumas < 0 && Mathf.Abs(pressedPos) > Mathf.Abs(yPosition))
-            //{
-            //    //yPosition = (float)(yPosition - pressedPos * 0.3);
-            //}
-            //gameObject.transform.position = new Vector3(Player.transform.position.x, yPosition, -10); // ATIMTI POZICIJA KURIA GAUSI IS RAY
-            //Debug.Log("Max pozicija : " + Screen.height + "Paliesta pozicija : " + touch.position.y / Screen.height);
+            scrolledY = touch.position.y - touchBeganAt - playerSize + gameObject.transform.position.y / 2;
+            scrolledY = scrolledY / 100 * strenght; // kameros keliavimo greitis
+            scrolledY = scrolledY - scrolledY / 2; // - camBeganAt
+            Debug.Log(camBeganAt);
         }
-        else //if (Mathf.Abs(gameObject.transform.position.y - Player.transform.position.y) > 0.02)
+        else
         {
-            float skirtumas = gameObject.transform.position.y - Player.transform.position.y;
-            float skirtumas2 = scrolledY - Player.transform.position.y;
-            //yPosition = (float)(yPosition - yPosition * returnSpeed);
             scrolledY = (float)(scrolledY - scrolledY * returnSpeed);
             touchBeganAt = 0;
-            gameObject.transform.position = new Vector3(Player.transform.position.x, scrolledY + hit.point.y, -10);
         }
-        //else
-        //{
-        //    yPosition = Player.transform.position.x;
-        //    gameObject.transform.position = new Vector3(Player.transform.position.x, yPosition, -10);
-        //}
+        camBeganAt = gameObject.transform.position.y * 0.8f;
+        camBeganAt = 0;
+        gameObject.transform.position = new Vector3(Player.transform.position.x, scrolledY + hit.point.y + camBeganAt, -10);
+        //Debug.Log(gameObject.transform.position.y);
     }
 }
