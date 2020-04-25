c77aab22774b2010bb481b1f340015eaa8d2e0ed
Karolis <karolisb27@gmail.com>
2020-04-26 01:23:58 +0300

    Flag not done

---
+++ zaidimai_ktu/Assets/Scripts/Objects/PowerUpExplosion.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpExplosion : MonoBehaviour
+{
+    [Range(0, 15)]
+    public float BouncingDelta = 0.05f;
+
+    [Range(0, 20)]
+    public float MaximumBounce = 1f;
+
+    public AudioClip sound;
+    public AudioSource source;
+
+    float currentPos;
+    float startPos;
+
+
+    int bounceDirection = 1;
+    private void Awake()
+    {
+        currentPos = gameObject.transform.position.y;
+        startPos = currentPos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(0, 0, 1);
+        currentPos += BouncingDelta * bounceDirection;
+        if (Mathf.Abs(startPos - currentPos) > MaximumBounce)
+        {
+            bounceDirection *= -1;
+        }
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, currentPos, gameObject.transform.position.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            source.PlayOneShot(sound, 1f);
+            currentPos = -100;
+            Invoke("RemoveObject", 10);
+        }
+
+    }
+    private void RemoveObject()
+    {
+        Destroy(gameObject);
+    }
+}
