909c3abb749d07063c2afe1c32b8d10a4d09d756
Karolis <karolisb27@gmail.com>
2020-05-16 21:24:14 +0300

    Level selection

--- zaidimai_ktu/Assets/Scripts/MenuManager.cs
+++ zaidimai_ktu/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
 
-    public GameObject Dropdown;
+    public Dropdown Dropdown;
 
     [Range(0f, 10f)]
     public float strenght = 1f;
 
-    public void StartGame ()
+    public void StartGame()
     {
         Debug.Log("Start");
-        SceneManager.LoadScene("KarolisScene");
+        if ((Dropdown.options[Dropdown.value].text) == "Karolis Level")
+            SceneManager.LoadScene("KarolisScene");
+        if ((Dropdown.options[Dropdown.value].text) == "Kajus Level")
+            SceneManager.LoadScene("KajusScene");
     }
 
     public void QuitGame()
